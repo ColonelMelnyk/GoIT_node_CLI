ad2f0e8f81e195960e7594ee86c687d64b928da1
ColonelMelnyk <119069975+ColonelMelnyk@users.noreply.github.com>
2023-08-28 22:58:11 +0300

    pt2

---
+++ index.js
@@ -0,0 +1,6 @@
+const currentTime = new Date();
+const hrs = currentTime.getHours();
+const min = currentTime.getMinutes();
+const sec = currentTime.getSeconds();
+console.log(`Current time: ${hrs}:${min}:${sec}`);
+console.log('And here I start my journey!')
