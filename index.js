ed00215d307095f26bd91b4a5e06abcc3131c952
ColonelMelnyk <119069975+ColonelMelnyk@users.noreply.github.com>
2023-08-29 17:02:04 +0300

    pt4

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const currentTime = new Date();
-const hrs = currentTime.getHours();
-const min = currentTime.getMinutes();
-const sec = currentTime.getSeconds();
-console.log(`Current time: ${hrs}:${min}:${sec}`);
-console.log('And here I start my journey!')
+
+// const currentTime = new Date();
+// const hrs = currentTime.getHours();
+// const min = currentTime.getMinutes();
+// const sec = currentTime.getSeconds();
+// console.log(`Current time: ${hrs}:${min}:${sec}`);
+// console.log('And here I start my journey!')
+
+const argv = require('yargs').argv;
+
+// TODO: рефакторити
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      // ...
+      break;
+
+    case 'get':
+      // ... id
+      break;
+
+    case 'add':
+      // ... name email phone
+      break;
+
+    case 'remove':
+      // ... id
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
