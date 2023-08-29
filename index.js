f0df6b1133e0e069ba715e9cd6a9cddc9d0284cb
ColonelMelnyk <119069975+ColonelMelnyk@users.noreply.github.com>
2023-08-29 19:14:06 +0300

    del comm

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-
-// const currentTime = new Date();
-// const hrs = currentTime.getHours();
-// const min = currentTime.getMinutes();
-// const sec = currentTime.getSeconds();
-// console.log(`Current time: ${hrs}:${min}:${sec}`);
-// console.log('And here I start my journey!')
 const contacts = require("./db/contacts");
 const yargs = require("yargs");
 const {hideBin} = require("yargs/helpers");
